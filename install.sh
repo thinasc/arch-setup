f7f22b01d0807982be3c93de3efc2e70855af301
Thiago Nascimento <tnascimento.thiago@gmail.com>
2023-08-14 09:59:04 +0100

    fix: filesystem

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
+echo "Partitioning"
 parted -s /dev/sda mklabel msdos
 parted /dev/sda mkpart primary 1MiB 100%
 # parted /dev/sda set 1 boot on
-# mkfs.ext4 /dev/sda1
+mkfs.ext4 /dev/sda1
+
+echo "Mounting"
 mount /dev/sda1 /mnt
 # mkdir /mnt/boot
 # mkdir /mnt/home
 
-pacstrap /mnt base linux
+echo "Pacstrapping"
+pacstrap /mnt base linux ansible
+
+echo "Generating fstab"
 genfstab /mnt >> /mnt/etc/fstab
 
+echo "Generating GRUB"
 arch-chroot /mnt bash -c 'pacman -Sy grub; grub-install /dev/sda; grub-mkconfig -o /boot/grub/grub.cfg'
-reboot
