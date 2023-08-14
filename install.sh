7f2bb7bc580c811cc0ec8205676e6f6af0a8dc18
Thiago Nascimento <tnascimento.thiago@gmail.com>
2023-08-14 10:46:21 +0100

    fix: bootloader

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 echo "Partitioning"
 parted -s /dev/sda mklabel msdos
 parted /dev/sda mkpart primary 1MiB 100%
-# parted /dev/sda set 1 boot on
 mkfs.ext4 /dev/sda1
 
 echo "Mounting"
@@ -13,7 +12,14 @@ echo "Pacstrapping"
 pacstrap /mnt base linux ansible
 
 echo "Generating fstab"
-genfstab /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
 
 echo "Generating GRUB"
-arch-chroot /mnt bash -c 'pacman -Sy grub; grub-install /dev/sda; grub-mkconfig -o /boot/grub/grub.cfg'
+arch-chroot /mnt
+mkinitcpio -p linux
+pacman -Syyuu grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Setup resolv.conf"
+echo 'nameserver 1.1.1.1' > /etc/resolv.conf
