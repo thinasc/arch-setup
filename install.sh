03be511011e08b4a45e66da72ef847fc4a07c0fc
Thiago Nascimento <tnascimento.thiago@gmail.com>
2023-08-21 00:43:48 +0100

    feat: add configs

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 echo "Partitioning"
 parted -s /dev/sda mklabel msdos
 # parted /dev/sda mkpart primary 1MiB 100%
-parted /dev/sda mkpart primary 1MiB 4GiB
-parted /dev/sda mkpart primary 4GiB 100%
+parted /dev/sda mkpart primary 1MiB 5GiB
+parted /dev/sda mkpart primary 5GiB 100%
 
 mkswap /dev/sda1
 mkfs.ext4 /dev/sda2
